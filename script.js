9ddbc35879c35a16e7af8e7fad2678690c829d49
vitor <vitorcastanhos@gmail.com>
2024-06-26 20:04:31 -0300

    changing UI and colors for the JS

--- script.js
+++ script.js
@@ -7,27 +7,48 @@ document.addEventListener("DOMContentLoaded", () => {
 		const savedTime = localStorage.getItem(`time-${input.id.split("-")[1]}`);
 		if (savedValue !== null) {
 			input.value = savedValue;
-			input.style.borderColor = "red";
+			input.style.borderColor = "var(--highlight-color)";
 		}
 		if (savedTime !== null) {
 			const timeSpan = document.querySelector(`#time-${input.id.split("-")[1]}`);
 			timeSpan.textContent = savedTime;
-			timeSpan.style.color = "red";
+			timeSpan.style.color = "var(--highlight-color)";
+
+			// Schedule the time color change if the saved time exists
+			const savedTimestamp = localStorage.getItem(`timestamp-${input.id.split("-")[1]}`);
+			if (savedTimestamp !== null) {
+				const elapsedTime = Date.now() - parseInt(savedTimestamp, 10);
+				if (elapsedTime < 3600000) {
+					setTimeout(() => {
+						timeSpan.style.color = "var(--alert-color)";
+					}, 3600000 - elapsedTime);
+				} else {
+					timeSpan.style.color = "var(--alert-color)";
+				}
+			}
 		}
 	});
 
 	inputs.forEach((input) => {
 		input.addEventListener("keypress", (event) => {
 			if (!isNaN(event.key) && event.key !== " ") {
-				input.style.borderColor = "red";
+				input.style.borderColor = "var(--highlight-color)";
 				const timeSpan = document.querySelector(`#time-${input.id.split("-")[1]}`);
 				const currentTime = new Date().toLocaleTimeString();
 				timeSpan.textContent = `Emprestado às: ${currentTime}`;
-				timeSpan.style.color = "red";
+				timeSpan.style.color = "var(--highlight-color)";
+
+				const timestamp = Date.now();
 
 				// Save data to localStorage
 				localStorage.setItem(input.id, input.value + event.key);
 				localStorage.setItem(`time-${input.id.split("-")[1]}`, timeSpan.textContent);
+				localStorage.setItem(`timestamp-${input.id.split("-")[1]}`, timestamp);
+
+				// Schedule the time color change
+				setTimeout(() => {
+					timeSpan.style.color = "var(--alert-color)";
+				}, 3600000);
 			}
 		});
 
@@ -41,6 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
 				// Remove data from localStorage
 				localStorage.removeItem(input.id);
 				localStorage.removeItem(`time-${input.id.split("-")[1]}`);
+				localStorage.removeItem(`timestamp-${input.id.split("-")[1]}`);
 			} else {
 				// Update localStorage with the current value
 				localStorage.setItem(input.id, input.value);
